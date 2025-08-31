c1ad1848ee581e1ffe4bb3eea7e905242ba76bb0
Kamran Alam <Kamrankhan0368@gmail.com>
2025-08-31 23:31:01 +0500

    update

---
+++ index.js
@@ -0,0 +1,5 @@
+var randm1 = Math.floor(Math.random()*6)+1;
+document.getElementById("dice1").src = "./images/dice"+randm1+".png"
+
+var randm2 = Math.floor(Math.random()*6)+1;
+document.getElementById("dice2").src = "./images/dice"+randm2+".png"
