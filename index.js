22581243889fa6c3d578ec616d361d60d1e13a5a
Kamran Alam <Kamrankhan0368@gmail.com>
2025-09-02 17:31:00 +0500

    Updated

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-var randm1 = Math.floor(Math.random()*6)+1;
-document.getElementById("dice1").src = "./images/dice"+randm1+".png"
+var randm1 = Math.floor(Math.random() * 6) + 1;
+document.getElementById("dice1").src = "./images/dice" + randm1 + ".png"
 
-var randm2 = Math.floor(Math.random()*6)+1;
-document.getElementById("dice2").src = "./images/dice"+randm2+".png"
+var randm2 = Math.floor(Math.random() * 6) + 1;
+document.getElementById("dice2").src = "./images/dice" + randm2 + ".png"
+
+function refreshRotate() {
+    document.getElementById("dice1").classList.add("reloder");
+    document.getElementById("dice2").classList.add("reloder");
+}
+
+refreshRotate();
